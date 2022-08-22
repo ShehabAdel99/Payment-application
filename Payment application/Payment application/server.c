9e9346b5238983d0234b9036baed3149c49e4d65
ShehabAdel99 <96583491+ShehabAdel99@users.noreply.github.com>
2022-08-22 19:45:27 +0200

    .

--- Payment application/Payment application/server.c
+++ Payment application/Payment application/server.c
@@ -7,113 +7,136 @@
 #include <string.h>
 #include <stdint.h>
 
-// Creating valid accounts DB
+// Creating a valid initialized accounts DB
 ST_accountsDB_t accounts[255] = {
 	{2000.0,RUNNING,"2564856474123698"},
-	{51313.2,BLOCKED,"6214785236541234"},
+	{5313.2,BLOCKED,"6214785236541234"},
 	{7851.5,RUNNING,"9874562155476666"},
 	{6848.55,RUNNING,"9856412766572214"},
-	{97451.56,BLOCKED,"9654775211443365"},
+	{9741.56,BLOCKED,"9654775211443365"},
 	{2874.9,BLOCKED,"2356821478652324"},
-	{978485.0,BLOCKED,"9632547812435547"},
+	{9785.0,BLOCKED,"9632547812435547"},
 	{1000.0,RUNNING,"96324587132365"},
 	{25.5,BLOCKED,"68741236985452"},
 	{9851.1,RUNNING,"99558824446663"},
 };
 
+
+// Creating a valid initialized accounts DB
+ST_transaction_t transactions[255] = { 0,0,DECLINED_INSUFFECIENT_FUND,0 };
+
+
 int index;      // gets needed account index in DB
-int trans = 0;
-
-
-ST_transaction_t transactions[255] = {0,0,DECLINED_INSUFFECIENT_FUND,0};
+int trans = 0;  // identifies the index of the transaction in transactions DB
 
 
 EN_transState_t recieveTransactionData(ST_transaction_t* transData)
 {
-	
+	//// Check Account Validation
 	if (isValidAccount(transData,accounts)== SERVER_OK)         
 	{
 
+		////Check if it is Blocked
+		if (isBlockedAccount(accounts) == BLOCKED_ACCOUNT) {
+			return DECLINED_STOLEN_CARD;
+		}
+
+		//// Check if the amount is available
 		if (isBelowMaxAmount(transData) == EXCEED_MAX_AMOUNT || transData->terminalData.transAmount > accounts[index].balance) {
 			return DECLINED_INSUFFECIENT_FUND;
 		}
-		else if (isBlockedAccount(accounts) == BLOCKED_ACCOUNT) {
-			return DECLINED_STOLEN_CARD;
-		}
-		else if (saveTransaction(transData)   == SAVING_FAILED)     
+		//else
+		//{
+		//                                                    /*just for testing*/
+		//	printf("Amount Accepted\n");     
+		//}
+
+		////Check if the transaction was saved
+		if (saveTransaction(transData)   == SAVING_FAILED)     
 		{
 			return INTERNAL_SERVER_ERROR;
 		}
-		else {
+		else 
+		{
 
-			//Updating DB with the new balance
+			////Updating DB with the new balance
 			accounts[index].balance = accounts[index].balance - transData->terminalData.transAmount;
-
+			//printf("Transaction Data was Recieved successfully\n");               //just for testing
 			return APPROVED;
 		}
 	}
 	else {
+		//printf("Transaction Data was NOT Recieved  ( FRAUD CARD )\n");               //just for testing
 		return FRAUD_CARD;
 	}
 };
 
 
 
-//Done
-EN_serverError_t isValidAccount(ST_CardData_t *cardData, ST_accountsDB_t* accountRefrence)
+EN_serverError_t isValidAccount(ST_CardData_t *cardData, ST_accountsDB_t *accountRefrence)
 {
 
 	for (int i = 0; i < 255; i++)
 	{
-		if (strcmp(cardData->PAN, accountRefrence[i].PAN) == 0) {      
-			index = i;
-			printf("found");
+
+		// Searching for the card PAN in the accounts DB
+		if (strcmp(cardData->PAN, accountRefrence[i].PAN) == 0) {  
+
+			index = i;               //Saving the index of the found account from DB to a global variable to be used throw code
+
+			//printf("Valid Account\n");      //just for testing
+
 			return SERVER_OK;
 		}
 	}
-		printf("not found");
+		//printf("ACCOUNT NOT FOUND\n");      //just for testing
 
 		return ACCOUNT_NOT_FOUND;
 	
 };
 
 
-//Done
+
 EN_serverError_t isBlockedAccount(ST_accountsDB_t* accountRefrence)
 {
 	if (accountRefrence[index].state == BLOCKED) {
+
+		//printf("BLOCKED ACCOUNT\n");        //just for testing
 		return BLOCKED_ACCOUNT;
 	}
 	else
 	{
+		//printf("NOT BLOCKED ACCOUNT\n");      //just for testing
 		return SERVER_OK;
 	}
 
 };
 
 
-//Done
+
 EN_serverError_t isAmountAvailabe(ST_terminalData_t* termData)
 {
 	if (termData->transAmount > accounts[index].balance) {
+		//printf("LOW BALANCE\n");        //just for testing
 		return LOW_BALANCE;
 	}
 	else
 	{
+		//printf("Sufficient balance\n");        //just for testing
 		return SERVER_OK;
 	}
 };
 
 
-//Done
+
 EN_serverError_t saveTransaction(ST_transaction_t* transData)
 {
-	// Saving transaction data to transactions DB
+	//// Saving transaction data to transactions DB
 	transactions[trans].cardHolderData = transData->cardHolderData;
 	transactions[trans].terminalData = transData->terminalData;
 	transactions[trans].transState = transData->transState;
 
-	// Giving a sequence number to a transaction
+	//// Giving a sequence number to a transaction
 	if (trans == 0)
 	{
 		transData->transactionSequenceNumber = 1;
@@ -122,48 +145,35 @@ EN_serverError_t saveTransaction(ST_transaction_t* transData)
 	{
 		transData->transactionSequenceNumber = (transactions[index - 1].transactionSequenceNumber + 1);
 	}
-	// Saving sequnce number
+	//// Saving sequnce number
 	transactions[index].transactionSequenceNumber = transData->transactionSequenceNumber;
 
-	trans++;
+	trans++; // Move to the next index in Transactions DB
 
-	// Checking if the transaction is saved
+	//// Checking if the transaction is saved
 	if (getTransaction(transData->transactionSequenceNumber, transData) == TRANSACTION_NOT_FOUND)
 	{
+		//printf("SAVING FAILED\n");        //just for testing
 		return SAVING_FAILED;
 	}
 	else
 	{
+		//printf("SAVING SUCCEEDED\n");        //just for testing
 		return SERVER_OK;
 	}
 };
 
 
-//Done
+
 EN_serverError_t getTransaction(uint32_t transactionSequenceNumber, ST_transaction_t* transData)
 {
 	for (int i = 0; i < 255; i++)
 	{
 		if (transData->transactionSequenceNumber == transactions[i].transactionSequenceNumber) {
-			printf("Transaction data is at index %d in the transactions DB.", i);
+			printf("Transaction data is at index %d in the transactions DB.\n", i);
 			return SERVER_OK;
 		}
 	}
+	//printf("TRANSACTION NOT FOUND\n");          //just for testing
 	return TRANSACTION_NOT_FOUND;
 }; 
-
-int main()
-
-{
-	ST_CardData_t* t = malloc(sizeof(t));
-
-	// isValidAccount function test
-	
-	//printf("entering valid account\n");
-	//strcpy(t->PAN, "2564856474123698");
-	//isValidAccount(t, accounts);
-	//printf("entering invalid account\n");
-	//strcpy(t->PAN, "4444");
-	//isValidAccount(t, accounts);
-
-}
